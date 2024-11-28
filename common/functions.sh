26c2c525c216e26356bc6409411b7c4c370ac65f
Simon <8466614+SimonBaars@users.noreply.github.com>
2024-11-28 21:34:21 +0100

    Update functions.sh

--- common/functions.sh
+++ common/functions.sh
@@ -148,7 +148,7 @@ mount_mirrors() {
 
 # Credits
 ui_print "**************************************"
-ui_print "*   MMT Extended by Zackptg5 @ XDA   *"
+ui_print " Magisk Auto Screenshot by SimonBaars "
 ui_print "**************************************"
 ui_print " "
 
